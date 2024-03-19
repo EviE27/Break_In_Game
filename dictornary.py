7317ab9d04367a6c7f8e81e49eb8bb9c9f148a32
EviE27 <158590030+EviE27@users.noreply.github.com>
2024-03-19 22:51:32 +0000

    I got rid of movement branch and coded it into the main

---
+++ dictornary.py
@@ -0,0 +1,24 @@
+"""you have been hird to steal the plans from a rives mod boss. Its late at night
+and you have arived at the enemys head quarters. you ned to look for a way to get
+in. You are able to move up, down, left and right, hit enter after you select your
+ direction."""
+start_tile = {}
+
+""" you are walking along the side of the building gray wear house. You come aros 
+   a door with a dirty window on the side of the building. Do you want to look
+   throught the window 
+    """
+side_door = {"door handel", "crow bar"}
+
+"""as you go down the frount of the building you come across a locked meadel trap 
+door you look and the lock has a number code, you look inforunt of you and see a 
+half coverd wooden crate.
+"""
+celler_entrance = {}
+
+"""You enter into the dark wear house. You look around to see many larger wooden
+crates. you see a door at the end the line of crates would. this is the room that
+you need to go to retive the plans. do you want to head right there or look around
+to see if 
+"""
+wear_house = {" "}
